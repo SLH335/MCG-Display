2449fc2b85bae00e5cf363cce415f8a86d78f0c4
SLH335 <114884726+SLH335@users.noreply.github.com>
2024-06-19 18:50:07 +0200

    feat: load credentials from .env file

--- services/util.services.go
+++ services/util.services.go
@@ -3,8 +3,12 @@ package services
 import (
 	"bytes"
 	"context"
+	"errors"
+	"fmt"
+	"os"
 
 	"github.com/a-h/templ"
+	"github.com/joho/godotenv"
 )
 
 func RenderComponent(component templ.Component) string {
@@ -12,4 +16,22 @@ func RenderComponent(component templ.Component) string {
 	component.Render(context.Background(), buf)
 
 	return buf.String()
+}
+
+func GetCredentials(n int) (username, password string, err error) {
+	err = godotenv.Load()
+	if err != nil {
+		return "", "", err
+	}
+
+	suffix := fmt.Sprintf("_%d", n)
+
+	username = os.Getenv("WEBUNTIS_USERNAME" + suffix)
+	password = os.Getenv("WEBUNTIS_PASSWORD" + suffix)
+
+	if username == "" && password == "" {
+		return "", "", errors.New("error: no credentials found in .env")
+	}
+
+	return username, password, nil
 }
